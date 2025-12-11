5a2a744b463617b5e8f2a1f6c806468e534dce38
TonNom <ton.email@example.com>
2025-12-11 20:12:59 +0000

    Initial commit Laravel Reservation Project with all components

---
+++ tailwind.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  content: ["./resources/**/*.blade.php","./resources/**/*.js","./resources/**/*.vue"],
+  theme:{extend:{colors:{primary:'#1E40AF',secondary:'#9333EA'}}},
+  plugins:[],
+}
